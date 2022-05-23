ec589d9ef2e15c4acbbf1af346e1360b9a404c19
Angelo <angelocsaverino@gmail.com>
2022-05-24 10:26:54 +1200

    save button works, saves one hour off though

--- develop/assets/script.js
+++ develop/assets/script.js
@@ -1,5 +1,13 @@
 var todaysDate = moment().format('MMMM d, YYYY')
-
+var workTimes = ['8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm'];
 
 // show todays date
 document.getElementById('currentDay').innerHTML = todaysDate;
+
+
+$(".saveBtn").on("click", function() {
+    var saveInfo = $(this).attr("data-id")
+
+
+    localStorage.setItem(saveInfo, $("#" + workTimes[saveInfo]).val())
+});
