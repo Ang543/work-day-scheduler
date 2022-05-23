c9e2f18b6e5cc892d2fe8c87d52dbec38e468ce5
Angelo <angelocsaverino@gmail.com>
2022-05-24 10:35:54 +1200

    integers were off by one, duh. does not save yet only console logs

--- develop/assets/script.js
+++ develop/assets/script.js
@@ -4,10 +4,9 @@ var workTimes = ['8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm
 // show todays date
 document.getElementById('currentDay').innerHTML = todaysDate;
 
-
+// save button functionality
 $(".saveBtn").on("click", function() {
     var saveInfo = $(this).attr("data-id")
-
 
     localStorage.setItem(saveInfo, $("#" + workTimes[saveInfo]).val())
 });
